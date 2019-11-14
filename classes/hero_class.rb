6c2a103d6f24b00fed716d9242d85d894da1a0ff
David Johnson <david_jono@hotmail.com>
2019-11-14 21:34:58 +1000

    update to location class and controller

--- classes/hero_class.rb
+++ classes/hero_class.rb
@@ -3,20 +3,13 @@ require 'colorize'
 class Hero
         
     @@initial_health = 100
-    @@initial_bladder = rand(10..40)
-    @@initial_belly = rand(10..40)
+    
 
         def self.initial_health
         @@initial_health
         end
 
-        def self.initial_bladder
-        @@initial_bladder
-        end
-
-        def self.initial_belly
-        @@initial_belly
-        end    
+        
     
         attr_reader :name, :health, :cash, :swagger_clout, :sure_footedness, :bladder, :belly
 
@@ -26,9 +19,11 @@ class Hero
         @swagger_clout = swagger_clout 
         @sure_footedness = sure_footedness 
                 
+        
+        @bladder = rand(1..40)
+        @belly = rand(1..40)
+
         @health = Hero.initial_health
-        @bladder = Hero.initial_bladder 
-        @belly = Hero.initial_belly
 
     end
 
@@ -36,10 +31,16 @@ class Hero
         puts "#{@name.upcase}'S STATS:".colorize(:red)
         puts "Health: #{@health}"
         puts "Cash: $#{@cash}"
-        puts "Swagger&Clout: #{@swagger_clout}"
+        puts "Swagger & Clout: #{@swagger_clout}"
         puts "Sure-Footedness: #{@sure_footedness}"
         puts "Bladder Level: #{@bladder}"
         puts "Belly Level: #{@belly}"
     end 
 
 end
+
+#hero's
+kev = Hero.new("Kev","150","90","90")
+douggie = Hero.new("Douggie","220","70","80") 
+robbo = Hero.new("Robbo","270","100","60")
+bazza = Hero.new("Bazza","120","50","50")
---
+++ controller.rb
@@ -0,0 +1,74 @@
+require './classes/hero_class.rb'
+require './classes/location_class.rb'
+
+puts pub1.name
+
+#Pubs and Streets
+# pub1 = Location.new("The Swanky Dank")
+
+
+
+#     puts "Welcome to the ultimate PubCrawl adventure game! 
+
+#             1. Rules
+#             2. View Hero's
+#             3. Play
+#                                 "
+
+#     answer = gets.chomp
+   
+#     until answer == "3" 
+   
+#         case answer
+    
+#         when "1"
+#             puts "THE RULES ARE SIMPLE!"  
+            
+#             puts "
+#             1. Return to title
+#             2. View Hero's
+#             3. Play"
+#                  answer = gets.chomp   
+#         when "2"
+#             puts "1. 
+#             #{kev.display_stats}"
+#             puts ""
+#             puts "2. 
+#             #{douggie.display_stats}"
+#             puts""
+#             puts "3. 
+#             #{robbo.display_stats}"
+#             puts""
+#             puts "4. 
+#             #{bazza.display_stats}"
+#             puts "who do you choose?"
+         
+#         when "3" 
+#             puts "You have just entered #{pub1.name}"
+#         #   pub method
+
+#         else 
+#             puts "unrecognized request"
+#         end
+
+# end
+    
+
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
